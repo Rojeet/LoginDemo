0212adebf4164207e71bb9eca348e84b5dc2ef36
@ssasinrojj <rojitmalakar62@gmail.com>
2017-11-09 15:04:31 +0545

    first third commit

--- app/src/main/java/com/example/lenovo/logindemo/MainActivity.java
+++ app/src/main/java/com/example/lenovo/logindemo/MainActivity.java
@@ -2,30 +2,65 @@ package com.example.lenovo.logindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-    private EditText Name;
-    private EditText Password;
-    private Button Login;
+        EditText username;
+        EditText password;
+
+        private String Name;
+        private String Password;
+
+        Button login;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            EditText editTextUserName = (EditText) findViewById(R.id.nametxt);
+            Name = editTextUserName.getText().toString();
+            EditText editTextPassword = (EditText)findViewById(R.id.passwordtxt);
+            Password = editTextPassword.getText().toString();
+            login = (Button) findViewById(R.id.loginbtn);
+
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setLogin();
+                }
+            });
 
 
 
+        }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Name = (EditText)findViewById(R.id.etname);
-        Login = (Button) findViewById(R.id.button);
+        public void setLogin() {
+            Name = username.getText().toString().trim();
+            Password = password.getText().toString().trim();
+            if (!validate()){
+                Toast.makeText(this, "Invalid Name",Toast.LENGTH_SHORT).show();
+            }else{
+                onSignupSuccess();
+            }
 
 
+        }
+        public boolean validate(){
+            boolean valid = true;
+            if (Name.isEmpty() || Password.isEmpty()) {
+                Toast.makeText(this, "Invalid Name", Toast.LENGTH_SHORT).show();
+                valid = false;
+            }
+            return valid;
+
+        }
+        public void onSignupSuccess(){
+            //TODO WHAT WILL GO AFTER SIGHNUP
+        }
     }
-    private void validate(String UserName, String UserPassword){
-        if ((UserName == "admin") && (UserPassword == "1234"))
-
-
-    }
-
-}
